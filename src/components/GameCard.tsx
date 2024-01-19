cdaf6cd9f36b14cd288d370dc4d2ef476ec1cfaf
karthikmocdoc <karthik@mocdoc.in>
2024-01-19 14:14:22 +0530

    Calling Small size Image Url to optimise images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIcons from "./PlatformIcons";
 import GameCriticScore from "./GameCriticScore";
+import getSmallImageUrl from "../services/imageUrl";
 
 interface Props {
   game: Game;
@@ -11,7 +12,7 @@ const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card borderRadius={10} overflow="hidden">
-        <Image src={game.background_image} />
+        <Image src={getSmallImageUrl(game.background_image)} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
           <HStack justifyContent="space-between">
