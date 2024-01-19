9312a572355e110c82b5d817ee70f0b3ae0935ac
karthikmocdoc <karthik@mocdoc.in>
2024-01-19 16:33:24 +0530

    Added skeleton for images to be displayed while loading

--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -1,9 +1,13 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GamesList = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+
+  const skeletonList = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -13,8 +17,16 @@ const GamesList = () => {
         spacing={10}
         padding={10}
       >
+        {isLoading &&
+          skeletonList.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
